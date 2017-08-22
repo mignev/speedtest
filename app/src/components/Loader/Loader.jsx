9bfb16d879997b62c74a67cd112c41a89eccf007
SashiDo <hello@sashido.io>
2017-08-22 23:17:16 +0300

    stage

---
+++ app/src/components/Loader/Loader.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+export default class Loader extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    const attributes = {
+      width: `${this.props.width}`,
+      height: `${this.props.height}`,
+      fill: this.props.fill,
+    };
+
+    if (this.props.customClassName) {
+      return (
+        <div className={this.props.customClassName}>
+          <svg {...attributes}>
+            <use xlinkHref={`#${this.props.name}`} />
+          </svg>
+        </div>
+      );
+    }
+
+    return (
+      <svg {...attributes}>
+        <use xlinkHref={`#${this.props.name}`} />
+      </svg>
+    );
+  }
+}
+
+Loader.propTypes = {
+  name: PropTypes.string.isRequired,
+  width: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired,
+  fill: PropTypes.string.isRequired,
+  customClassName: PropTypes.string.isRequired,
+};
+
+Loader.defaultProps = {
+  fill: 'transparent',
+  width: '100%',
+  height: '100%',
+  customClassName: '',
+};
--- app/src/routes.jsx
+++ app/src/routes.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Root from './containers/Root/Root';
 import Home from './containers/Home/Home';
 import Results from './containers/Results/Results';
 
 export default () => (
   <Root>
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/:id" component={Results} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   </Root>
 );
